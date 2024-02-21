4ca4a74b998cfaf22e45240b0784a4e075a22456
wassasd <154278218+wassasd@users.noreply.github.com>
2024-02-21 19:42:16 +0200

    Update main.py
    Removed extra spaces

--- main.py
+++ main.py
@@ -6,10 +6,8 @@ def main():
     print(f"--- Begin report of {book_name} ---")
     print(f"{book_name} has: {words} words")
     print()
-
     letter_report(count_letters(text.lower()))
     print("--- End of Report ---")
-
 
 def get_book_text(path):
     with open(path) as f:
@@ -17,7 +15,6 @@ def get_book_text(path):
     
 def word_count(word_input):
     words = word_input.split()
-
     return len(words)
 
 def count_letters(text_input):
@@ -31,6 +28,5 @@ def count_letters(text_input):
 def letter_report(text):
     for i in list(text):
         print(f"The '{i}' character was found {text[i]} times")
-
 
 main()
